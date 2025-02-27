69835c31b97e515aa7af57b11efaf8c1c8af2167
DheerajBathi <dheerajbathi.com>
2025-02-27 14:35:35 +0530

    Initial commit

---
+++ t2s.py
@@ -0,0 +1,14 @@
+import pyttsx3
+
+engine=pyttsx3.init()
+
+# with open("Text_to_Speech/sample.txt", "r", encoding="utf-8") as file:
+with open("sample.txt", "r", encoding="utf-8") as file:
+
+    text=file.read()
+    # print(text)
+
+# engine.say("Hello I am Python")
+engine.say(text)
+engine.runAndWait()
+engine.stop()
