2417c97a22983f6ad3a1387530c87e15620c8a11
Dheeraj <159713697+DheerajBathi@users.noreply.github.com>
2025-03-01 11:12:04 +0530

    Update t2s.py

--- t2s.py
+++ t2s.py
@@ -1,11 +1,11 @@
 import pyttsx3
 
 engine=pyttsx3.init()
-
+text=input("Enter a message here: ")
 # with open("Text_to_Speech/sample.txt", "r", encoding="utf-8") as file:
-with open("sample.txt", "r", encoding="utf-8") as file:
+# with open("sample.txt", "r", encoding="utf-8") as file:
 
-    text=file.read()
+#     text=file.read()
     # print(text)
 
 # engine.say("Hello I am Python")
